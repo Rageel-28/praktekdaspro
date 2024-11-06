cf1adc815a3a32ad64982aefd82f41e1c2d1c66a
JustinLaiya <faesalgali@gmail.com>
2024-11-06 08:20:17 +0700

    Percobaan 1

--- daspro-jobsheet9/ArrayNilai19.java
+++ daspro-jobsheet9/ArrayNilai19.java
@@ -1,13 +1,11 @@
 import java.util.Scanner; 
 public class ArrayNilai19 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {xxx
         Scanner sc = new Scanner(System.in);
         int [] nilaiAkhir = new int[10];
 
-        for (int i = 0; i<nilaiAkhir.length; i++) {
-            System.out.print("Masukan nilai akhir ke - "+ i + "  :");
-            nilaiAkhir[i] = sc.nextInt();
-        }
+         
+        
         for (int i=0; i<nilaiAkhir.length; i++) {
             if (nilaiAkhir [i]> 70 ) {
                 System.out.println("Mahasiswa ke - " + i +" lulus!");
