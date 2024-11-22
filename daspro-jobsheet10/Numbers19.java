84148d7ec408d8bc49c301a4a6beb84cf91be070
JustinLaiya <faesalgali@gmail.com>
2024-11-22 18:18:13 +0700

    percobaan 1

--- daspro-jobsheet10/Numbers19.java
+++ daspro-jobsheet10/Numbers19.java
@@ -11,7 +11,7 @@ public class Numbers19 {
         System.out.println(Arrays.toString(myNumbers[i]));
      }
      for (int i = 0 ; i <myNumbers.length; i ++) {
-        System.out.println("Panjang baris ke - " + (i+1) +" : " + myNumbers[i].length);
+        System.out.println("Panjang baris ke - " + (i+1) +": " + myNumbers[i].length);
      }
     }
 }
